6b20b222f2e50bc8d241898062fa9d445fe6bdd4
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-08-30 15:03:34 +0600

    added bigger number

---
+++ graterNumber.js
@@ -0,0 +1,12 @@
+/*Write a program to determine the greater of two numbers.
+*/
+
+const num1 = 324;
+const num2 = 443825;
+
+if (num1 > num2) {
+    console.log("bigNumber = ", num1);
+}
+else{
+    console.log("bigNumber = ",num2)
+}
