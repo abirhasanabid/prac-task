08508ba1014d88b0ba413595106984083f0b54da
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-08-30 14:53:17 +0600

    added a programe whice is finding the number positive or not

---
+++ +ve_-ve.js
@@ -0,0 +1,12 @@
+/* Write a program that determines whether a given number is positive or negative.
+*/
+
+
+const num = -1;
+
+if(num >= 0){
+    console.log("+ve");
+}
+else{
+    console.log("-ve");
+}
